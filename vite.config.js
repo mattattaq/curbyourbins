3372c9706b6b24ed8b069bceab41982dc2b42ef4
MattAttaq <mattatttaq@gmail.com>
2025-03-09 01:15:41 -0500

    [ build ]

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default {
+  base: "/curb-your-bins/", // Replace with your GitHub repository name
+  build: {
+    outDir: "dist",
+  },
+};
