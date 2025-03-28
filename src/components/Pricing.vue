b4615dbedd91a78ae41ef28ab4b01bdce9219d6c
Mathew Allbright <>
2025-03-27 23:12:14 -0400

    [ nit ] - removed "former" from the text

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -7,7 +7,7 @@
             <div class="half fade-in">
                 <h2>Pricing</h2>
                 <p><b>Who are you and why do you want my trash?</b></p>
-                <p>I’m Matthew Allbright, a former software developer who was recently laid off. While searching for my
+                <p>I’m Matthew Allbright, a software developer who was recently laid off. While searching for my
                     next opportunity, I figured I’d offer my assistance to my neighbors. I don’t <i>want</i> your trash
                     per
                     se — I just want to make your life a little easier. I’ll take your trash to the curb on Sunday
